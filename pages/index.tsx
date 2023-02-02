6175b3b7113cb0bb1de0c5c02194ebb8440976c5
Bishal Neupane <Neupanebishal07@gmail.com>
2023-02-02 13:45:57 +0545

    Update tweet status

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,7 +27,7 @@ export default function Home() {
       >
         <motion.a
           variants={FADE_DOWN_ANIMATION_VARIANTS}
-          href="https://twitter.com/steventey/status/1613928948915920896"
+          href="https://twitter.com/bishaltwt/status/1621054927278841857"
           target="_blank"
           rel="noreferrer"
           className="mx-auto mb-5 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full bg-blue-100 px-7 py-2 transition-colors hover:bg-blue-200"
